fbc4f9c3b7438968f6158384d45d17ad3aca1a1d
ktpark <>
2022-02-07 00:32:35 +0900

    .

---
+++ src/main/java/yuri/piggybackWagon/PiggybackWagonApplication.java
@@ -0,0 +1,13 @@
+package yuri.piggybackWagon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggybackWagonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggybackWagonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yuri/piggybackWagon/PiggybackWagonApplicationTests.java
@@ -0,0 +1,13 @@
+package yuri.piggybackWagon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggybackWagonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
